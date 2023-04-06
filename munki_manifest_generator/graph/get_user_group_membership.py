132cc5c44c96efb499c193594ee0ebc8c8e7914d
Tobias Almén <78877636+almenscorner@users.noreply.github.com>
2023-04-06 10:00:00 +0200

    Remove graph calls and lookup from batch

--- munki_manifest_generator/graph/get_user_group_membership.py
+++ munki_manifest_generator/graph/get_user_group_membership.py
@@ -1,59 +1,67 @@
 #!/usr/bin/env python3
 
+import logging
+
+from munki_manifest_generator.logger import logger
+
 """
 This module is used to get the user group membership and update included manifests.
 """
 
-from munki_manifest_generator.graph.make_api_request import make_api_request
 
 ENDPOINT = "https://graph.microsoft.com/v1.0/users"
 
 
-def get_user_group_membership(token, upn, groups, current_manifests, device_manifest):
+def get_user_group_membership(responses, groups, current_manifests, device_manifest):
     """Returns a list of group names the user is a member of and updates the included manifests."""
 
-    aad_user_object_id = None
-    q_param_user = {"$filter": "userPrincipalName eq '%s'" % upn}
-    user_object = make_api_request(ENDPOINT, token, q_param_user)
+    user = device_manifest.__dict__["user"]
+    serial_number = device_manifest.__dict__["serialnumber"]
+    memberOf = [val for val in responses if user in val["userPrincipalName"] for val in val["value"]]
 
-    for id in user_object["value"]:
-        object_id = id["id"]
-        aad_user_object_id = object_id
-    q_param_group = {"$select": "id,displayName"}
-
-    # If Azure AD user id is none, skip getting groups
-    if aad_user_object_id is None:
-        print("AAD User ID is null, skipping user group memberships")
-
-    else:
-        memberOf = make_api_request(
-            ENDPOINT + "/" + aad_user_object_id + "/transitiveMemberOf", token, q_param_group
-        )
-
+    if memberOf:
         user_groups = []
         user_groups_name = []
 
-        for group_id in memberOf["value"]:
+        for group_id in memberOf:
             id = group_id["id"]
             user_groups.append(id)
             user_groups_name.append(group_id["displayName"])
+
+        if logger.isEnabledFor(logging.DEBUG):
+            logger.debug(
+                "[%s] User found in groups: %s",
+                serial_number,
+                ", ".join(user_groups_name),
+            )
+            logger.debug(
+                "[%s] Groups from JSON or list: %s",
+                serial_number,
+                str(groups),
+            )
 
         for group in groups:
             if group["type"] == "user":
                 if group["id"] in user_groups:
                     if group["name"] in current_manifests:
                         if group["name"] not in device_manifest.included_manifests:
-                            print(
-                                "User found in group for "
-                                + group["name"]
-                                + ", adding included manifest for group"
+                            logger.info(
+                                "[%s] User found in group for %s, adding included manifest for group",
+                                serial_number,
+                                group["name"],
                             )
                             device_manifest.included_manifests.append(group["name"])
                     else:
-                        print(
-                            "User found in group for "
-                            + (group["name"])
-                            + " but manifest does not exist, skipping"
+                        logger.info(
+                            "[%s] User found in group for %s but manifest does not exist, skipping",
+                            serial_number,
+                            group["name"],
                         )
 
         return user_groups_name
+
+    else:
+        logger.warning(
+            "[%s] User not found in any groups",
+            serial_number,
+        )
